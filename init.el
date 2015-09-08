63953525e65cf11b876f9c592c1896c6033858e4
kqr <k@rdw.se>
2015-09-08 22:01:59 +0200

    made init.el compatible with graphical emacs

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 (use-package evil
   :ensure t
   :init (evil-mode 1)
-  :config (setq evil-echo-state nil))
+  :config (setq evil-echo-state t))
 
 
 (menu-bar-mode -1)
@@ -30,19 +30,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe-mode 0 nil (fringe))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Luxi Mono" :foundry "b&h" :slant normal :weight normal :height 128 :width normal :background "black" :foreground "beige"))))
  '(font-lock-builtin-face ((t nil)))
- '(font-lock-comment-face ((t (:foreground "color-243"))))
+ '(font-lock-comment-face ((t (:foreground "dim gray"))))
  '(font-lock-constant-face ((t nil)))
  '(font-lock-function-name-face ((t nil)))
- '(font-lock-keyword-face ((t (:foreground "color-208"))))
- '(font-lock-string-face ((t (:foreground "color-106"))))
- '(font-lock-type-face ((t (:foreground "color-31"))))
+ '(font-lock-keyword-face ((t (:foreground "peru"))))
+ '(font-lock-string-face ((t (:foreground "OliveDrab3"))))
+ '(font-lock-type-face ((t (:foreground "dodger blue"))))
  '(font-lock-variable-name-face ((t nil)))
- '(mode-line ((t (:background "color-235" :foreground "color-242" :box (:line-width -1 :style released-button)))))
- '(mode-line-inactive ((t (:inherit mode-line :background "black" :foreground "color-240" :box (:line-width -1 :color "grey75") :weight light)))))
+ '(mode-line ((t (:background "gray11" :foreground "dim gray"))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "black" :foreground "dim grey")))))
