cd81ccde9c74e0ba0aba2f2eaae6984d62a62c31
kqr <k@rdw.se>
2017-09-24 00:33:05 +0200

    hilight god mode!

--- init.el
+++ init.el
@@ -234,7 +234,14 @@
   (setq-default cursor-type 'box)
 
   (defun god-mode-update-cursor ()
-    (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+    (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar))
+    (cond
+     (god-local-mode
+      (progn (set-face-background 'mode-line "default")
+	     (set-face-foreground 'mode-line "default")))
+     (t
+      (progn (set-face-background 'mode-line "dark orange")
+	     (set-face-foreground 'mode-line "black")))))
   
   (add-hook 'god-mode-enabled-hook 'god-mode-update-cursor)
   (add-hook 'god-mode-disabled-hook 'god-mode-update-cursor))
