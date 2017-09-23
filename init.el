c9dd0d3c2dbe9a21a540499eb799fc525fcab9ee
kqr <k@rdw.se>
2017-09-24 00:47:44 +0200

    better order...

--- init.el
+++ init.el
@@ -237,11 +237,11 @@
     (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar))
     (cond
      (god-local-mode
-      (progn (set-face-background 'mode-line "default")
-	     (set-face-foreground 'mode-line "default")))
-     (t
       (progn (set-face-background 'mode-line "dark orange")
-	     (set-face-foreground 'mode-line "black")))))
+	     (set-face-foreground 'mode-line "black")))
+     (t
+      (progn (set-face-background 'mode-line "default")
+	     (set-face-foreground 'mode-line "default")))))
   
   (add-hook 'god-mode-enabled-hook 'god-mode-update-cursor)
   (add-hook 'god-mode-disabled-hook 'god-mode-update-cursor))
