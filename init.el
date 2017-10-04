7b93e9ef9e145b5e5a822a4489012fec0ee58287
kqr <k@rdw.se>
2017-10-04 21:10:21 +0200

    even better modularisation

--- init.el
+++ init.el
@@ -27,47 +27,26 @@
 
 ;;; Code:
 
-
-;;; Various more sane defaults
-(setq-default major-mode 'text-mode)
-
-(setq-default make-backup-files nil)
-(setq-default large-file-warning-threshold 100000000)
-
-;; You want this because otherwise Emacs will mix tabs and spaces...
-(customize-set-variable 'indent-tabs-mode nil)
-
-
-;;; Prerequisites:
-
-(require 'package)
-(push '("marmalade" . "http://marmalade-repo.org/packages/") package-archives)
-(push '("melpa stable" . "http://stable.melpa.org/packages/") package-archives)
-(push '("melpa" . "http://melpa.milkbox.net/packages/") package-archives)
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package no-littering :init
-  (setq backup-inhibited t)
-  (setq auto-save-default nil))
-
 (defun load-directory (dir)
   "Load all *.el files located in DIR."
   (let ((load-it
 	 (lambda (f) (load-file (concat (file-name-as-directory dir) f)))))
     (mapc load-it (directory-files dir nil "\\.el$"))))
 
+(package-initialize)
 
-(customize-set-variable 'custom-file "~/.emacs.d/config/local/custom.el")
+(setq custom-file "~/.emacs.d/config/local/custom.el")
+(load custom-file)
+
 (customize-set-variable 'custom-theme-directory "~/.emacs.d/themes/")
-
 (load-directory "~/.emacs.d/config/")
 (load-directory "~/.emacs.d/config/local/")
 
+(setq-default major-mode 'text-mode)
+(setq-default make-backup-files nil)
+(setq-default large-file-warning-threshold 100000000)
 
+;; You want this because otherwise Emacs will mix tabs and spaces...
+(customize-set-variable 'indent-tabs-mode nil)
 
 ;;; init.el ends here
