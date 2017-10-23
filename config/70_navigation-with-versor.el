6964abca4b05995dfd855831c58cae7abd9b6377
kqr <k@rdw.se>
2017-10-23 16:51:08 +0200

    some more fix maybe?

--- config/70_navigation-with-versor.el
+++ config/70_navigation-with-versor.el
@@ -11,16 +11,24 @@
    'meta
    'ctrl-x
    'text-in-code
-   'quiet-underlying)
+   'quiet-underlying
+   'local)
 
   (customize-set-variable 'versor-auto-change-for-modes nil)
   (customize-set-variable 'versor-move-out-when-at-end nil)
   (customize-set-variable 'versor-level-wrap nil)
   (customize-set-variable 'versor-meta-level-wrap nil)
+  (customize-set-variable 'versor-mode-current-levels
+                          (mapcar 'versor-mode-levels-triplet
+                                  '((prog-mode "program" "exprs")
+                                    ("prog-mode" "strutured text" "words"))))
   (customize-set-variable 'versor-text-level 2)
+  (customize-set-variable 'versor-text-meta-level 4)
+  (customize-set-variable 'versor-non-text-level 2)
+  (customize-set-variable 'versor-non-text-meta-level 6)
 
-  (setq versor-meta-dimensions-valid-for-modes
-        '((t t "cartesian" "structured text" "program")))
+  (customize-set-variable 'versor-meta-dimensions-valid-for-modes
+                          '((t t "cartesian" "structured text" "program")))
   
   (seq-doseq (metamovemap (seq-subseq moves-moves 1))
     (seq-doseq (movemap (seq-subseq metamovemap 1))
