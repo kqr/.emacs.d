ff3c28a6e7fa6fcc1f233146be6c9dcdfe7487e5
kqr <k@rdw.se>
2018-05-18 17:17:57 +0200

    don't show attributes default sunrise

--- init.el
+++ init.el
@@ -645,6 +645,8 @@
 ;;; Orthodox file manager
 (autoload 'sunrise-cd "sunrise-commander")
 (define-key global-map (kbd "<f2>") 'sunrise-cd)
+(with-eval-after-load "sunrise-commander"
+  (setq sr-show-file-attributes nil))
 
 ;;; Git integration
 (autoload 'magit-status "magit")
