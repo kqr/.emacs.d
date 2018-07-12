ae93a2b935cb255489af7c22d742b8bd32f3a3f8
kqr <a@xkqr.org>
2018-07-12 21:06:07 +0200

    add cfengine mode

--- init.el
+++ init.el
@@ -752,6 +752,12 @@
   (add-hook 'ansible-hook 'ansible::auto-decrypt-encrypt))
 
 
+;;;; CFEngine mode
+(autoload 'cfengine3-mode "cfengine")
+(push '("\\.cf\\'" . cfengine3-mode) auto-mode-alist)
+(with-eval-after-load "cfengine"
+  (add-hook 'cfengine3-mode-hook 'eldoc-mode))
+
 
 ;;; Calculator
 (autoload 'calc "calc")
