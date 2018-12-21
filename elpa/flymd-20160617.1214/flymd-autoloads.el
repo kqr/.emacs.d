e8a8032c13bb72e08149a19ab15d91ee1c413e4d
Christoffer Stjernlöf <christoffer@loop54.com>
2018-12-21 15:28:45 +0100

    flymd + set browser specially on OS X

---
+++ elpa/flymd-20160617.1214/flymd-autoloads.el
@@ -0,0 +1,31 @@
+;;; flymd-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymd" "flymd.el" (0 0 0 0))
+;;; Generated autoloads from flymd.el
+
+(autoload 'flymd-flyit "flymd" "\
+Enable realtime markdown preview on the current buffer.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymd" '("flymd-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("flymd-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymd-autoloads.el ends here
