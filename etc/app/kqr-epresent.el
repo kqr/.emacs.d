7e00448ccbb7d6c4fe79549962503bbe0b58ca02
kqr <a@xkqr.org>
2019-06-18 21:16:25 +0200

    Bug fix for epresent when opening scratch in new frame

--- etc/app/kqr-epresent.el
+++ etc/app/kqr-epresent.el
@@ -1,2 +1,26 @@
 (when (require 'epresent)
-  (add-to-list 'evil-emacs-state-modes 'epresent-mode))
+  (add-to-list 'evil-emacs-state-modes 'epresent-mode) ;; only works sometimes???
+  (setq epresent-text-scale 120)
+
+  (define-advice epresent--get-frame
+      (:around (actual-get-frame &rest args) epresent-frame-set-buffer)
+    (let* ((presentation-buffer (current-buffer))
+           (presentation-frame (apply actual-get-frame args)))
+      (message "presentation buffer: %S" presentation-buffer)
+      (message "presentation frame: %S" presentation-frame)
+      (message "epresent-frame: %S" epresent--frame)
+      (with-selected-frame epresent--frame
+        (switch-to-buffer presentation-buffer))
+      epresent--frame)))
+
+;; Bad place for this but I can't be arsed to come up with a better one
+;; (defun remove-all-advice-for (symbol)
+;;   (interactive "a")
+;;   (let ((removed-advices 0))
+;;     (advice-mapc
+;;      (lambda (advice props)
+;;        (when advice
+;;          (incf removed-advices)
+;;          (advice-remove symbol advice)))
+;;      symbol)
+;;     (message "Removed %d pieces of advice" removed-advices)))
