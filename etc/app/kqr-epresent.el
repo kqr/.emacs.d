9b6af70491c27072c13dbb20aa5328947b0022ee
Christoffer Stjernlöf <christoffer@loop54.com>
2019-06-19 14:46:38 +0200

    Bug fix for epresent with redrawing EVERYTHING on slide change

--- etc/app/kqr-epresent.el
+++ etc/app/kqr-epresent.el
@@ -4,23 +4,25 @@
 
   (define-advice epresent--get-frame
       (:around (actual-get-frame &rest args) epresent-frame-set-buffer)
-    (let* ((presentation-buffer (current-buffer))
-           (presentation-frame (apply actual-get-frame args)))
-      (message "presentation buffer: %S" presentation-buffer)
-      (message "presentation frame: %S" presentation-frame)
-      (message "epresent-frame: %S" epresent--frame)
-      (with-selected-frame epresent--frame
-        (switch-to-buffer presentation-buffer))
-      epresent--frame)))
+    (let ((presentation-buffer (current-buffer)))
+      (apply actual-get-frame args)
+      (switch-to-buffer presentation-buffer)
+      epresent--frame))
+
+  (define-advice redraw-display
+      (:around (actual-redraw &rest args) epresent-only-redisplay-frame)
+    (if (eq major-mode 'epresent-mode)
+        (apply 'redraw-frame (cons nil args))
+      (apply actual-redraw args))))
 
 ;; Bad place for this but I can't be arsed to come up with a better one
-;; (defun remove-all-advice-for (symbol)
-;;   (interactive "a")
-;;   (let ((removed-advices 0))
-;;     (advice-mapc
-;;      (lambda (advice props)
-;;        (when advice
-;;          (incf removed-advices)
-;;          (advice-remove symbol advice)))
-;;      symbol)
-;;     (message "Removed %d pieces of advice" removed-advices)))
+(defun remove-all-advice-for (symbol)
+  (interactive "a")
+  (let ((removed-advices 0))
+    (advice-mapc
+     (lambda (advice props)
+       (when advice
+         (incf removed-advices)
+         (advice-remove symbol advice)))
+     symbol)
+    (message "Removed %d pieces of advice" removed-advices)))
