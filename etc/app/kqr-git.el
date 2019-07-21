c2147ffb1e7e9d134a0b45bd4b237574f0d038ad
Christoffer Stjernlöf <christoffer@loop54.com>
2019-07-21 12:51:30 +0200

    Refine hunks in magit diff

--- etc/app/kqr-git.el
+++ etc/app/kqr-git.el
@@ -2,5 +2,6 @@
 (define-key global-map (kbd "<f3>") 'magit-status)
 (with-eval-after-load "magit"
   (setq magit-log-margin '(t age magit-log-margin-width t 10))
+  (setq magit-diff-refine-hunks 'all)
   (setq magit-diff-arguments '("--stat" "--no-ext-diff" "--diff-algorithm=histogram"))
   (setq magit-log-arguments '("-n64" "--graph" "--decorate" "--color" "--date-order")))
