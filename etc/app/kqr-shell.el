41e0ce770ff6eb7e3605c61446625eb847d4437f
Christoffer Stjernlöf <christoffer@loop54.com>
2019-08-05 16:42:11 +0200

    Set up a bind for find-file-under-point in eshell!

--- etc/app/kqr-shell.el
+++ etc/app/kqr-shell.el
@@ -2,4 +2,22 @@
   (require 'em-smart)
 
   (setq eshell-where-to-jump 'begin
-        eshell-review-quick-commands nil))
+        eshell-review-quick-commands nil)
+
+  (defun get-filename-under-point ()
+    "Return a very rough guess at what the filename under point is."
+    (interactive)
+    (save-excursion
+      (let ((filename-end (progn (forward-symbol 1) (point)))
+            (filename-beginning (progn (forward-symbol -1) (point))))
+        (buffer-substring filename-beginning filename-end))))
+
+  (defun find-file-under-point ()
+    "Attempt to open whatever filename is under point."
+    (interactive)
+    (find-file-other-window (concat default-directory (get-filename-under-point))))
+
+  (defun configure-eshell ()
+    (define-key eshell-mode-map (kbd "C-c f") 'eshell/find-file-under-point))
+
+  (add-hook 'eshell-mode-hook 'configure-eshell))
