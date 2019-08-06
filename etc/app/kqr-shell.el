a00e2edd8eb78075d87cb2781bd708cd64d7841a
Christoffer Stjernlöf <christoffer@loop54.com>
2019-08-06 14:09:30 +0200

    Set up a bind to open Eshell in current directory

--- etc/app/kqr-shell.el
+++ etc/app/kqr-shell.el
@@ -4,9 +4,14 @@
   (setq eshell-where-to-jump 'begin
         eshell-review-quick-commands nil)
 
+  (defun get-current-directory ()
+    "Get the currently visited directory in this buffer."
+    (if (buffer-file-name)
+        (file-name-directory (buffer-file-name))
+      default-directory))
+
   (defun get-filename-under-point ()
     "Return a very rough guess at what the filename under point is."
-    (interactive)
     (save-excursion
       (let ((filename-end (progn (forward-symbol 1) (point)))
             (filename-beginning (progn (forward-symbol -1) (point))))
@@ -15,9 +20,24 @@
   (defun find-file-under-point ()
     "Attempt to open whatever filename is under point."
     (interactive)
-    (find-file-other-window (concat default-directory (get-filename-under-point))))
+    (find-file-other-window (concat (get-current-directory) (get-filename-under-point))))
+
+  (defun eshell-open-current-directory ()
+    "Open Eshell navigated to the directory of the current buffer in Eshell."
+    (interactive)
+    (let* ((parent (get-current-directory))
+           (height (/ (window-total-height) 3))
+           (name (car (last (split-string parent "/" t)))))
+      (split-window-vertically (- height))
+      (other-window 1)
+      (eshell "new")
+      (rename-buffer (concat "*eshell: " name "*"))))
 
   (defun configure-eshell ()
-    (define-key eshell-mode-map (kbd "C-c f") 'eshell/find-file-under-point))
+    (company-mode -1)
+    (define-key eshell-mode-map (kbd "C-c f") 'eshell/find-file-under-point)
+    (with-eval-after-load "evil"
+      ;; Ctrl-D to send EOF in Eshell
+      (evil-define-key '(normal insert) eshell-mode-map (kbd "C-d") 'delete-window)))
 
   (add-hook 'eshell-mode-hook 'configure-eshell))
