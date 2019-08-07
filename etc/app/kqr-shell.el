224b5dc8ce5ba824d563fe9ac1219af33dbb91b9
Christoffer Stjernlöf <christoffer@loop54.com>
2019-08-07 14:41:17 +0200

    Additional Eshell improvements: scrolling and eshell-bol in Evil

--- etc/app/kqr-shell.el
+++ etc/app/kqr-shell.el
@@ -33,17 +33,34 @@
       (eshell "new")
       (rename-buffer (concat "*eshell: " name "*"))))
 
-  (defun configure-eshell ()
-    (company-mode -1)
-    (define-key eshell-mode-map (kbd "C-c f") 'eshell/find-file-under-point)
-    (with-eval-after-load "evil"
-      ;; Ctrl-D to send EOF in Eshell
-      (evil-define-key '(normal insert) eshell-mode-map (kbd "C-d") 'delete-window)))
-
-  (add-hook 'eshell-mode-hook 'configure-eshell)
-
+  ;; Things that only make sense in combination with evil
   (with-eval-after-load "evil"
+    (defun eshell-evil-insert-line ()
+      "Go to the beginning of the prompt and enter insert mode."
+      (interactive)
+      (eshell-bol)
+      (evil-insert-state 1))
+
     ;; This makes it very convenient to open an Eshell, but it disables the
     ;; default shell command running bind. If desired, it can probably be
     ;; bound to C-! or something.
-    (evil-define-key '(normal) 'global (kbd "!") 'eshell-open-current-directory)))
+    (evil-define-key '(normal) 'global (kbd "!") 'eshell-open-current-directory))
+
+  (defun configure-eshell ()
+    ;; Configure normal Emacs scrolling in Eshell, instead of keeping cursor centered
+    (set (make-local-variable 'maximum-scroll-margin) 0.25)
+    (set (make-local-variable 'scroll-margin) 0)
+
+    ;; Turn off modes that make less sense in a shell
+    (company-mode -1)
+
+    ;; Set up some custome binds
+    (define-key eshell-mode-map (kbd "C-c f") 'eshell/find-file-under-point)
+
+    (with-eval-after-load "evil"
+      (evil-define-key 'normal eshell-mode-map (kbd "0") 'eshell-bol)
+      (evil-define-key 'normal eshell-mode-map (kbd "I") 'eshell-evil-insert-line)
+      ;; Ctrl-D to send EOF in Eshell
+      (evil-define-key '(normal insert) eshell-mode-map (kbd "C-d") 'delete-window)))
+
+  (add-hook 'eshell-mode-hook 'configure-eshell))
