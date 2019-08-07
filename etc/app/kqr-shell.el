6a3fe3386bcb4bed732780a5b8a5595ffe4becbd
Christoffer Stjernlöf <christoffer@loop54.com>
2019-08-07 15:20:23 +0200

    More Eshell conveniences

--- etc/app/kqr-shell.el
+++ etc/app/kqr-shell.el
@@ -4,23 +4,30 @@
   (setq eshell-where-to-jump 'begin
         eshell-review-quick-commands nil)
 
-  (defun get-current-directory ()
+  (defun buffer-current-directory ()
     "Get the currently visited directory in this buffer."
     (if (buffer-file-name)
         (file-name-directory (buffer-file-name))
       default-directory))
 
-  (defun get-filename-under-point ()
+  (defun buffer-filename-at (point)
     "Return a very rough guess at what the filename under point is."
     (save-excursion
+      (when point (goto-char point))
       (let ((filename-end (progn (forward-symbol 1) (point)))
             (filename-beginning (progn (forward-symbol -1) (point))))
-        (buffer-substring filename-beginning filename-end))))
+        (buffer-substring-no-properties filename-beginning filename-end))))
 
-  (defun find-file-under-point ()
+  (defun buffer-find-file-at (point)
     "Attempt to open whatever filename is under point."
-    (interactive)
-    (find-file-other-window (concat (get-current-directory) (get-filename-under-point))))
+    (interactive "d")
+    (find-file-other-window (concat (buffer-current-directory)
+                                    (buffer-filename-at nil))))
+
+  (defun buffer-find-file-at-mouse-click (event)
+    "Attempt to open whatever filename is clicked in the buffer."
+    (interactive "e")
+    (buffer-find-file-at (posn-point (event-end event))))
 
   (defun eshell-open-current-directory ()
     "Open Eshell navigated to the directory of the current buffer in Eshell."
@@ -55,11 +62,17 @@
     (company-mode -1)
 
     ;; Set up some custome binds
-    (define-key eshell-mode-map (kbd "C-c f") 'eshell/find-file-under-point)
+    (define-key eshell-mode-map (kbd "C-c f") 'buffer-find-file-at)
+    ;; I'm hesitant to enable this since I'm worried it will replace the default
+    ;; action. I could, perhaps, advice whatever the default action is, but eh.
+    ;; We'll see.
+    ;;(define-key eshell-mode-map (kbd "<mouse-1>") 'buffer-find-file-at-mouse-click)
 
     (with-eval-after-load "evil"
       (evil-define-key 'normal eshell-mode-map (kbd "0") 'eshell-bol)
       (evil-define-key 'normal eshell-mode-map (kbd "I") 'eshell-evil-insert-line)
+      (evil-define-key 'normal eshell-mode-map (kbd "RET") 'eshell-send-input)
+
       ;; Ctrl-D to send EOF in Eshell
       (evil-define-key '(normal insert) eshell-mode-map (kbd "C-d") 'delete-window)))
 
