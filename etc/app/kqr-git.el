beec18025cae822296d2049ee3fbe42673fb84ad
Christoffer Stjernlöf <christoffer@loop54.com>
2019-08-21 12:20:41 +0200

    Reestore visibility of foldable Magit sections when opening again

--- etc/app/kqr-git.el
+++ etc/app/kqr-git.el
@@ -4,4 +4,7 @@
   (setq magit-log-margin '(t age magit-log-margin-width t 10))
   (setq magit-diff-refine-hunks 'all)
   (setq magit-diff-arguments '("--stat" "--no-ext-diff" "--diff-algorithm=histogram"))
-  (setq magit-log-arguments '("-n64" "--graph" "--decorate" "--color" "--date-order")))
+  (setq magit-log-arguments '("-n64" "--graph" "--decorate" "--color" "--date-order"))
+
+  ;; Restore visibility of foldable sections when opening again
+  (setq magit-section-cache-visibility nil))
