bef46eb8ce63f866ed1150fb0338a0231ce70061
Christoffer Stjernlöf <christoffer@loop54.com>
2019-08-30 14:54:03 +0200

    Improve defaults to magit-log and magit-diff! Finally!!

--- etc/app/kqr-git.el
+++ etc/app/kqr-git.el
@@ -1,10 +1,20 @@
 (autoload 'magit-status "magit")
 (define-key global-map (kbd "<f3>") 'magit-status)
 (with-eval-after-load "magit"
+  ;; Enable all subcommands and options everywhere
+  (setq transient-default-level 7)
+
   (setq magit-log-margin '(t age magit-log-margin-width t 10))
   (setq magit-diff-refine-hunks 'all)
-  (setq magit-diff-arguments '("--stat" "--no-ext-diff" "--diff-algorithm=histogram"))
-  (setq magit-log-arguments '("-n64" "--graph" "--decorate" "--color" "--date-order"))
+  ;; I used to try to set magit-log-arguments and magit-diff-arguments here,
+  ;; but I have since learned that those are set in etc/transient/values.el!
+  ;; And I pretty much always want to use those unless otherwise specified.
+  (setq magit-use-sticky-arguments nil)
 
   ;; Restore visibility of foldable sections when opening again
-  (setq magit-section-cache-visibility nil))
+  (setq magit-section-cache-visibility nil)
+
+  ;; Backup uncommitted WIP changes to prevent serious foot-shooting
+  (magit-wip-mode +1)
+
+  (setq magit-no-confirm '(safe-with-wip)))
---
+++ etc/transient/values.el
@@ -0,0 +1,2 @@
+((magit-diff:magit-diff-mode "--ignore-all-space" "--diff-algorithm=histogram" "--no-ext-diff" "--stat")
+ (magit-log:magit-log-mode "-n64" "--no-merges" "--date-order" "--graph" "--color" "--decorate"))
