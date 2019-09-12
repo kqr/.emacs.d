842eb157acbab6416fe73bbe60f4a85adb35593e
Christoffer Stjernlöf <christoffer@loop54.com>
2019-09-12 14:14:19 +0200

    Update text-mode editorconfig override

--- etc/kqr-editorconfig.el
+++ etc/kqr-editorconfig.el
@@ -1,11 +1,15 @@
 (when (require 'editorconfig)
-  (defun use-default-fill-column-in-text-mode (props)
+  (defun use-reasonable-fill-column-in-text-mode (props)
     (when (derived-mode-p 'text-mode)
-      (puthash 'max_line_length "80" props)))
+      (let ((max-line-length
+             (string-to-number (gethash 'max_line_length props "80"))))
+        (when (< 120 max-line-length)
+          (puthash 'max_line_length "120" props)))))
 
   (editorconfig-mode 1)
 
   (setq editorconfig-trim-whitespaces-mode 'ws-butler-mode)
 
-  (add-hook 'editorconfig-hack-properties-functions
-            'use-default-fill-column-in-text-mode))
+  (dolist (override '(use-reasonable-fill-column-in-text-mode))
+    (add-hook 'editorconfig-hack-properties-functions
+              override)))
