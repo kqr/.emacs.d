889eae8981ba4deec1738f3aa0332a76e27fa079
Christoffer Stjernlöf <christoffer@loop54.com>
2019-09-12 13:44:21 +0200

    Set up fancy graph for short log in magit-status too

--- etc/transient/values.el
+++ etc/transient/values.el
@@ -1,2 +1,3 @@
 ((magit-diff:magit-diff-mode "--ignore-all-space" "--diff-algorithm=histogram" "--no-ext-diff" "--stat")
- (magit-log:magit-log-mode "-n64" "--no-merges" "--date-order" "--graph" "--color" "--decorate"))
+ (magit-log:magit-log-mode "-n64" "--no-merges" "--date-order" "--graph" "--color" "--decorate")
+ (magit-log:magit-status-mode "-n64" "--date-order" "--graph" "--color" "--decorate"))
