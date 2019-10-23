d143fc8ac1d94d91ec8b4759473354e5f924a7b0
Christoffer Stjernlöf <christoffer@loop54.com>
2019-10-23 14:25:51 +0200

    Try getting better indentation in js2-mode
    (Although this probably didn't do it, it seems like the right thing to do anyway)

--- etc/kqr-editorconfig.el
+++ etc/kqr-editorconfig.el
@@ -1,4 +1,5 @@
 (when (require 'editorconfig)
+
   (defun use-reasonable-fill-column-in-text-mode (props)
     (when (derived-mode-p 'text-mode)
       (let ((max-line-length
@@ -6,10 +7,19 @@
         (when (< 120 max-line-length)
           (puthash 'max_line_length "120" props)))))
 
+  (defun apply-indentation-to-all-modes (props)
+    (let ((indent-size (string-to-number (gethash 'indent_size props "4"))))
+      (message "Setting indent size to %S" indent-size)
+      (setq c-basic-offset indent-size
+            js-indent-level indent-size
+            js2-basic-offset indent-size
+            sgml-basic-offset indent-size)))
+
   (editorconfig-mode 1)
 
   (setq editorconfig-trim-whitespaces-mode 'ws-butler-mode)
 
-  (dolist (override '(use-reasonable-fill-column-in-text-mode))
+  (dolist (override '(use-reasonable-fill-column-in-text-mode
+                      apply-indentation-to-all-mode))
     (add-hook 'editorconfig-hack-properties-functions
               override)))
