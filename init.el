ff96ca6ce95f1e9857e190eae336b79d03e9e1d0
Chris S <a@xkqr.org>
2024-06-01 10:38:42 +0200

    I don't practically use Latex nor fountain nowadays

--- init.el
+++ init.el
@@ -805,23 +805,6 @@
 ;; key I have configured for avy.
 
 
-;; Latex editing.
-(progn
-  ;; TODO verify what's required for this that I'm missing
-  ;;(require 'tex-site nil)
-
-  (setq-default font-latex-deactivated-keyword-classes
-                '("textual" "type-command" "type-declaration"))
-
-  (defun configure-latex ()
-    "Configure AUCTeX in mode hooks."
-    (setq-default TeX-PDF-mode t)
-    (setq-default TeX-engine 'xetex)
-    (setq-default TeX-auto-save t)
-    (require 'latex-preview-pane)
-    (latex-preview-pane-mode))
-
-  (add-hook 'LaTeX-mode-hook #'configure-latex))
 
 (use-package drag-stuff
   :config
@@ -837,11 +820,6 @@
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
   :mode (("\\.md\\'" . markdown-mode)))
-
-;;; Writing prose...
-(use-package fountain-mode
-  :commands (fountain-mode)
-  :mode (("\\.fountain\\'" . fountain-mode)))
 
 ;;; Programming
 (use-package flycheck
