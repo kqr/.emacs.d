db414a839b92d036109eb801b8c3320dc9c572c9
Chris S <a@xkqr.org>
2024-06-01 10:40:02 +0200

    I don't care much for company for personal use

--- init.el
+++ init.el
@@ -632,51 +632,6 @@
 
   (global-aggressive-indent-mode +1))
 
-;; Completion with company mode (hopefully practically intrusion-free)
-;; This might not work the way I want to because it IS intrusive. Disabled until
-;; I have had time to fix it.
-(use-package company
-  :disabled
-  :defer 6
-  :diminish company-mode
-  :config
-  (global-company-mode +1)
-
-  (defun company-complete-common-or-selected ()
-    "Insert the common part, or if none, complete using selection."
-    (interactive)
-    (when (company-manual-begin)
-      (if (not (equal company-common company-prefix))
-          (company--insert-candidate company-common)
-        (company-complete-selection))))
-
-  (setq company-frontends
-        '(company-pseudo-tooltip-unless-just-one-frontend
-          company-preview-if-just-one-frontend
-          company-echo-metadata-frontend))
-  ;; Setting 0 idle delay was a nice idea, but it's way too slow for that
-  ;; Someone reports 0.3 working okay with omnisharp too, but let's ramp
-  ;; up slightly more slowly, to get maximum mileage out of it too.
-  (setq company-idle-delay 0.1)
-  (setq company-tooltip-align-annotations t)
-
-
-  ;; We don't want completion to prevent us from actually navigating the code
-  (define-key company-active-map (kbd "<return>") nil)
-  (define-key company-active-map (kbd "C-p") nil)
-  (define-key company-active-map (kbd "<up>") nil)
-  (define-key company-active-map (kbd "C-n") nil)
-  (define-key company-active-map (kbd "<down>") nil)
-  (define-key company-active-map (kbd "C-<up>") #'company-select-previous)
-  (define-key company-active-map (kbd "C-<down>") #'company-select-next)
-  (define-key company-active-map (kbd "TAB")
-    #'company-complete-common-or-cycle))
-
-(use-package company-posframe
-  :after (company)
-  :config
-  (company-posframe-mode +1))
-
 ;;; Miscellaneous interaction
 ;; Export window contents to neat HTML
 (use-package htmlize
@@ -803,7 +758,6 @@
 ;; - https://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs.html
 ;; Removed because I rarely use it, it confuses me, and it conflicts with the
 ;; key I have configured for avy.
-
 
 
 (use-package drag-stuff
